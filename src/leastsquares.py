b2d93541cf06f3fc2006e894660534dce03647b1
LuizHenriqueMartinsDias <luiz.henrique.m.dias07@gmail.com>
2026-08-05 12:46:52 -0300

    refactor: altera o nome dos métodos da classe para fit e predict

--- src/leastsquares.py
+++ src/leastsquares.py
@@ -6,30 +6,28 @@ arquivo = BASE_DIR / "data" / "toy_dataset"
 TOY_DATASET = pd.read_csv(arquivo)
 
 class LinearRegression:
-    def __init__(self,train_dataset,val_dataset):
+    def __init__(self):
         self.b = None
         self.m = None
-        self.train_dataset = train_dataset
-        self.val_dataset = val_dataset
 
-    def get_param(self):
-        sumx = self.train_dataset["x"].sum()
-        sumy = self.train_dataset["y"].sum()
-        sumxy = (self.train_dataset["x"] * self.train_dataset["y"]).sum()
-        sumx2 = (self.train_dataset["x"] ** 2).sum()
-        n = len(self.train_dataset)
+    def fit(self,train_dataset):
+        sumx = train_dataset["x"].sum()
+        sumy = train_dataset["y"].sum()
+        sumxy = (train_dataset["x"] * train_dataset["y"]).sum()
+        sumx2 = (train_dataset["x"] ** 2).sum()
+        n = len(train_dataset)
 
         self.m = (n * sumxy - sumx * sumy) / (n * sumx2 - sumx ** 2)
         self.b = (sumy - self.m * sumx) / n
 
-    def linear_function(self,x):
-        self.get_param()
+    def predict(self,x):
         y = self.m * x + self.b
         print(y)
 
 def main():
-    lr = LinearRegression(TOY_DATASET,TOY_DATASET)
-    lr.linear_function(1)
+    lr = LinearRegression()
+    lr.fit(TOY_DATASET)
+    lr.predict(1)
 
 if __name__ == "__main__":
     main()
