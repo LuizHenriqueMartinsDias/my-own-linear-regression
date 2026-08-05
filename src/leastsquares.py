c5cbb046bfaf7f1d92f58411332b10227ccb7c20
LuizHenriqueMartinsDias <luiz.henrique.m.dias07@gmail.com>
2026-08-05 12:21:26 -0300

    feat: implementa funçao de calcular coeficiente angular e linear

--- src/leastsquares.py
+++ src/leastsquares.py
@@ -1,2 +1,34 @@
+from pathlib import Path
+
+import pandas as pd
+BASE_DIR = Path(__file__).resolve().parent.parent
+arquivo = BASE_DIR / "data" / "toy_dataset.csv"
+TOY_DATASET = pd.read_csv(arquivo)
+
 class LinearRegression:
-    def calc_coeff(self, X, Y):
+    def __init__(self,train_dataset,val_dataset):
+        self.b = None
+        self.m = None
+        self.train_dataset = train_dataset
+        self.val_dataset = val_dataset
+
+    def get_param(self):
+        sumx = sum(self.train_dataset["x"].values)
+        sumy = sum(self.train_dataset["y"].values)
+        sumxy = sum(self.train_dataset["x"].values * self.train_dataset["y"].values)
+        sumxs = sum(self.train_dataset["x"] ** 2)
+        n = len(self.train_dataset["x"])
+        self.m = n * sumxy - sumx * sumy/n * sumxs - (sumx**2)
+        self.b = sumy - self.m * sumx/n
+
+    def linear_function(self,x):
+        self.get_param()
+        y = self.m * x + self.b
+        print(y)
+
+def main():
+    lr = LinearRegression(TOY_DATASET,TOY_DATASET)
+    lr.linear_function(1)
+
+if __name__ == "__main__":
+    main()
